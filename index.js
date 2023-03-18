8da0f4045003f23c6fabed6ca0a5b3874bc62850
hadimakki387 <hmakki387@gmail.com>
2023-03-18 12:50:26 +0200

    recieved data from dog-walker page , added them to databse, created a session for them

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 const fs = require("fs");
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require('uuid');
 
 // Set view engine
 app.set("view engine", "ejs");
@@ -24,7 +26,7 @@ app.use(
     secret: "mySecret",
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false, maxAge: 1000 * 60 * 60 },
+    
   })
 );
 
@@ -46,10 +48,15 @@ const dogOwnerSchema = new mongoose.Schema({
 
 // Define dog walker schema
 const dogWalkerSchema = new mongoose.Schema({
-  name: String,
+  Fname: String,
+  Lname: String,
   email: String,
   password: String,
   address: String,
+  telNumber: Number,
+  address:String,
+  age: Number,
+  id:String,
   availability: [String],
   specialSkills: [String],
 });
@@ -105,9 +112,7 @@ app.get("/", (req, res) => {
 
 // Render sign-in page
 app.get("/signIn", (req, res) => {
-  
     res.render('signIn');
-  
 });
 
 // After successful sign-in, render sign-up page if it is a new user or
@@ -198,6 +203,47 @@ app.get("/dog-walker", (req, res) => {
   res.render("dogWalkerSignUp");
 });
 
+
+app.post('/dog-walker',async (req,res)=>{
+  const {Fname,Lname,password,Email,Tel} = req.body
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const fullName = Fname +" "+ Lname
+  const id = uuidv4()
+  const dogWalker = new DogWalker({
+    Fname:Fname,
+    Lname:Lname,
+    password:hashedPassword,
+    email:Email,
+    telNumber:Tel,
+    id:id
+  })
+
+  DogWalker.findOne({email:Email})
+    .then(foundUser=>{
+      if(foundUser){
+        const user = {
+          name:foundUser.Lname + " " + foundUser.Fname,
+          id:foundUser.id
+        }
+        req.session.user = user
+      }else{
+        dogWalker.save()
+        const user = {
+          name:fullName,
+          id : id
+        }
+        req.session.user = user
+      }
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+
+    res.send("hello")
+    
+  
+})
+
 app.post("/signIn", (req, res) => {
   res.render("signIn"); // Render view for sign in page
 });
@@ -209,10 +255,7 @@ app.post(
   (req, res) => {
     // Handles posting walk data
     let id = req.user.id
-    let address = req.body.address;
-    let dogsName = req.body.dogName;
-    let dogBreed = req.body.dogBreed;
-    let name = req.user.displayName;
+    const {address,dogsName,dogBreed,name} = req.body
     const imgBuffered = req.file.buffer;
 
     const walkPost = new walkingPost({
--- public/style.js
+++ public/style.js
@@ -1,52 +1,104 @@
-
-
-
 // This function is to creat slide animantion to the humburger menu on the mobile verision
-$("button.mobile-menu-button").click(function() {
+$("button.mobile-menu-button").click(function () {
   $(".mobile-menu").toggleClass("slide");
-  if($('.mobile-menu').hasClass('slide')){
-    $('.mobile-menu').slideDown()
-  }else{
-    $('.mobile-menu').slideUp()
+  if ($(".mobile-menu").hasClass("slide")) {
+    $(".mobile-menu").slideDown();
+  } else {
+    $(".mobile-menu").slideUp();
   }
-  
 });
-
 
 // This function is to give style to the links in the navbar whenever they are clicked
 $(".links").click(function () {
-    $(".links").not(this).removeClass("text-emerald-500 border-b-4 border-emerald-500");
-    $(this).addClass("text-emerald-500 border-b-4 border-emerald-500");
-    if ($(this).hasClass("text-emerald-500 border-b-4 border-emerald-500")) {
-      $(".links").not(this).removeClass("text-emerald-500 border-b-4 border-emerald-500");
-    }
-  });
+  $(".links")
+    .not(this)
+    .removeClass("text-emerald-500 border-b-4 border-emerald-500");
+  $(this).addClass("text-emerald-500 border-b-4 border-emerald-500");
+  if ($(this).hasClass("text-emerald-500 border-b-4 border-emerald-500")) {
+    $(".links")
+      .not(this)
+      .removeClass("text-emerald-500 border-b-4 border-emerald-500");
+  }
+});
 
+// This function is to hide the signIn buttons in the signIn page and unhide the signIn by email form
+$(".email").click(() => {
+  $(".signInButtons").addClass("hidden");
+  $(".emailSignIn").removeClass("hidden");
+  $(".newUser").addClass("hidden");
+});
 
-  // This function is to hide the signIn buttons in the signIn page and unhide the signIn by email form
-  $('.email').click(()=>{
-    $('.signInButtons').addClass('hidden')
-    $('.emailSignIn').removeClass('hidden')
-    $('.newUser').addClass('hidden')
-    
-  })
+// This function is to unhide the signIn buttons in the signIn page and hide the signIn by email form
+$(".backBtn").click(() => {
+  $(".signInButtons").removeClass("hidden");
+  $(".emailSignIn").addClass("hidden");
+  $(".newUser").removeClass("hidden");
+});
 
+// This function is to creat slide animantion to the humburger menu on the mobile verision
+$(".homeBurger").click(() => {
+  $(".leftSilder").toggleClass("slideIn ");
+  $(".bgChange").toggleClass("brightness-50 blur-sm");
+  if ($(".leftSilder").hasClass("slideIn")) {
+    $(".leftSilder").animate({ left: "0px" });
+  } else {
+    $(".leftSilder").animate({ left: "-384px" });
+  }
+});
 
-  // This function is to unhide the signIn buttons in the signIn page and hide the signIn by email form
-  $('.backBtn').click(()=>{
-    $('.signInButtons').removeClass('hidden')
-    $('.emailSignIn').addClass('hidden')
-    $('.newUser').removeClass('hidden')
-  })
+const passwordField = $(".password");
+const passwordWarning = $(".password-warning");
 
+function validatePassword() {
+  const password = passwordField.val();
+  const passwordRegex =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])/;
 
-  // This function is to creat slide animantion to the humburger menu on the mobile verision
-  $('.homeBurger').click(()=>{ 
-    $('.leftSilder').toggleClass('slideIn '); 
-    $('.bgChange').toggleClass('brightness-50 blur-sm') 
-    if ($('.leftSilder').hasClass('slideIn')) { 
-      $('.leftSilder').animate({ left: '0px' }); 
-    } else { 
-      $('.leftSilder').animate({ left: '-384px' }); 
-    } })
-  
+  if (password.length < 8 || !passwordRegex.test(password)) {
+    passwordField.get(0).setCustomValidity("invalid");
+    passwordWarning.text(
+      "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+    );
+  } else {
+    passwordField.get(0).setCustomValidity("");
+    passwordWarning.text("");
+  }
+}
+
+passwordField.on("input", validatePassword);
+
+const emailField = $(".email");
+const emailWarning = $(".email-warning");
+
+function validateEmail() {
+  const email = emailField.val();
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{3})+$/;
+
+  if (!emailRegex.test(email)) {
+    emailField.get(0).setCustomValidity("invalid");
+    emailWarning.text("Please enter a valid email address.");
+  } else {
+    emailField.get(0).setCustomValidity("");
+    emailWarning.text("");
+  }
+}
+
+emailField.on("input", validateEmail);
+
+const phoneField = $(".phone");
+const phoneWarning = $(".phone-warning");
+
+function validatePhone() {
+  const phone = phoneField.val();
+  const phoneRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+  if (!phoneRegex.test(phone)) {
+    phoneField.get(0).setCustomValidity("invalid");
+    phoneWarning.text("Please enter a valid phone number.");
+  } else {
+    phoneField.get(0).setCustomValidity("");
+    phoneWarning.text("");
+  }
+}
+
+phoneField.on("input", validatePhone);
